bdea4f31b887799d7b931a78b4d6508549a41175
Park Changyong <tempus157@gmail.com>
2022-05-13 11:12:27 +0900

    Add vector

---
+++ core/vector.cpp
@@ -0,0 +1,5 @@
+#include "vector.hpp"
+
+Vector::Vector() : x(0), y(0) {}
+
+Vector::Vector(int x, int y) : x(x), y(y) {}
---
+++ core/vector.hpp
@@ -0,0 +1,14 @@
+#ifndef H_VECTOR
+#define H_VECTOR
+
+struct Vector
+{
+    int x;
+    int y;
+
+public:
+    Vector();
+    Vector(int x, int y);
+}
+
+#endif
