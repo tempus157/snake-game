8a9e0ba600b0199e8244cf1f5576f8ab62ab4fa5
Park Changyong <tempus157@gmail.com>
2022-05-13 14:50:54 +0900

    Write example

--- core/main.cpp
+++ core/main.cpp
@@ -2,17 +2,27 @@
 
 int main()
 {
+    WINDOW *win1;
+
     initscr();
+    resize_term(25, 25);
     start_color();
-    init_pair(1, COLOR_RED, COLOR_WHITE);
+    init_pair(1, COLOR_WHITE, COLOR_RED);
 
-    attron(COLOR_PAIR(1));
-    printw("Hello, world!");
-    attroff(COLOR_PAIR(1));
-
+    border('*', '*', '*', '*', '*', '*', '*', '*');
+    mvprintw(1, 1, "A default window");
     refresh();
     getch();
-    endwin();
 
+    win1 = newwin(20, 20, 3, 3);
+    wbkgd(win1, COLOR_PAIR(1));
+    wattron(win1, COLOR_PAIR(1));
+    mvwprintw(win1, 1, 1, "A new window");
+    wborder(win1, '@', '@', '@', '@', '@', '@', '@', '@');
+    wrefresh(win1);
+
+    getch();
+    delwin(win1);
+    endwin();
     return 0;
 }
