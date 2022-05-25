2e7367e1f11e01b3e3c77709bfe5e203e01eb3e2
Park Changyong <tempus157@gmail.com>
2022-05-26 02:14:46 +0900

    Remove all

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,4 @@
-#include "../framework/core/app.hpp"
-#include "./windows/mainWindow.hpp"
-
 int main()
 {
-    return App::create()
-        ->useWindow(new MainWindow())
-        ->execute();
+    return 0;
 }
