510f47605cd357805ad1f4eadac1cb212d61a92a
Park Changyong <tempus157@gmail.com>
2022-05-26 02:37:23 +0900

    Implement Object

--- framework/core.cpp
+++ framework/core.cpp
@@ -1,5 +1,39 @@
 #include "core.hpp"
 
+const Vector Vector::zero = Vector(0, 0);
+const Vector Vector::one = Vector(1, 1);
+const Vector Vector::up = Vector(0, 1);
+const Vector Vector::down = Vector(0, -1);
+const Vector Vector::left = Vector(-1, 0);
+const Vector Vector::right = Vector(1, 0);
+
+Vector::Vector() {}
+Vector::Vector(int x, int y) : x(x), y(y) {}
+
+Object &Object::create()
+{
+    return *new Object();
+}
+
+Object &Object::setText(std::string text)
+{
+    this->text = text;
+    return *this;
+}
+
+Object &Object::setPosition(Vector position)
+{
+    this->position = position;
+    return *this;
+}
+
+Object &Object::setPosition(int x, int y)
+{
+    position.x = x;
+    position.y = y;
+    return *this;
+}
+
 App &App::create()
 {
     return *new App();
--- framework/core.hpp
+++ framework/core.hpp
@@ -2,12 +2,49 @@
 #define __FRAMEWORK_CORE__
 
 #include <functional>
+#include <string>
+
+struct Vector
+{
+public:
+    int x = 0;
+    int y = 0;
+
+    static const Vector zero;
+    static const Vector one;
+    static const Vector up;
+    static const Vector down;
+    static const Vector left;
+    static const Vector right;
+
+    Vector();
+    Vector(int x, int y);
+};
+
+class Object
+{
+public:
+    static Object &create();
+    Object &setText(std::string text);
+    Object &setPosition(Vector position);
+    Object &setPosition(int x, int y);
+
+private:
+    std::string text;
+    Vector position;
+};
+
+class Window
+{
+public:
+    static Window &create();
+};
 
 class App
 {
 public:
     static App &create();
-    App &useWindow(std::function<void()> window);
+    App &useWindow(std::function<Window()> window);
     int execute();
 };
 
