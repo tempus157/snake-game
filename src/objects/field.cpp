c64f66d31940ed0e5bd2f050e74f95fd02787831
Park Changyong <tempus157@gmail.com>
2022-06-01 01:49:46 +0900

    Print wall in unicode

--- src/objects/field.cpp
+++ src/objects/field.cpp
@@ -2,6 +2,18 @@
 
 #include <array>
 
+string cellToString(int cell) {
+    switch (cell) {
+    case 0:
+        return " ";
+    case 1:
+        return "□";
+    case 2:
+        return "■";
+    }
+    return "?";
+}
+
 Object field() {
     auto data = Property<std::array<std::array<int, 25>, 25>>({{
         {2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
@@ -36,7 +48,7 @@ Object field() {
             return $for(row, [=](int cell, int j) {
                 return $union({
                     $goto(Vector2(i * 2, j)),
-                    label(std::to_string(cell)),
+                    label(cellToString(cell)),
                 });
             });
         }),
